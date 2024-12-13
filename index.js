09645b9866828532d65ad8e2eecd6921c35446ff
Aboubacar SANGARE <abouba.sang@outlook.com>
2024-12-13 08:22:05 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
+
+app.get("/payment-success", (req, res) => {
+  const redirectUrl = "myapp://(payment)/success";
+  res.redirect(redirectUrl);
+});
+
+app.get("/payment-cancelled", (req, res) => {
+  const redirectUrl = "myapp://(payment)/cancelled";
+  res.redirect(redirectUrl);
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
